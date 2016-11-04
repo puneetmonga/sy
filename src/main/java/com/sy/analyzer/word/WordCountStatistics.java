32b08c15d2df1670e68e5d71fc2e3dc34c5348a0
puneetmonga <puneet@sequoiaheritage.com>
2016-11-03 19:13:22 -0700

    - One more comment.

--- src/main/java/com/sy/analyzer/word/WordCountStatistics.java
+++ src/main/java/com/sy/analyzer/word/WordCountStatistics.java
@@ -12,17 +12,19 @@ import static java.util.Comparator.comparing;
 import static java.util.Comparator.comparingInt;
 
 /**
+ * Class represent the output of {@link WordCountAnalyzer}.
+ * It relies on {@link BinaryMap} to store the word counts sorted (first on word length and then on alphabetical/natural order).
  * Created by pmonga on 11/2/16.
  */
 public class WordCountStatistics implements Analysis {
     private BinaryMap<Word, Integer> countMap = new BinaryMap<>(WORD_COMPARATOR);
-    public static final Comparator<Word> WORD_COMPARATOR = comparingInt(Word::getLength).thenComparing(comparing(Word::getValue));
+    private static final Comparator<Word> WORD_COMPARATOR = comparingInt(Word::getLength).thenComparing(comparing(Word::getValue));
 
     public Integer get(Word word) {
         return Optional.ofNullable(countMap.get(word)).orElse(0);
     }
 
-    public void set(Word word, int count) {
+    void set(Word word, int count) {
         countMap.put(word, count);
     }
 
